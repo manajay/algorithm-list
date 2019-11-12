a01fd6134750e3a46f09653f951bd1f8a45788e1
icourt <icourt@icourtdeMacBook-Pro.local>
2019-11-12 10:46:02 +0800

    update median-of-two-sorted-arrays

--- 4-findMedianSortedArray/findMedianSortedArrays.cpp
+++ 4-findMedianSortedArray/findMedianSortedArrays.cpp
@@ -35,9 +35,17 @@ PS. 这是拼多多 客户端三面面试题，要求白板代码，大家可以
   * 
  */
 
-#include <stdio.h>
 #include <iostream>
+#include <stdio.h>
 #include <vector>
+#include <cstdint> 
+#include <stdint.h>
+#include <limits>
+#include <climits>
+#include <string>      
+#include <sstream> 
+#include <iomanip>
+
 using namespace std;
 
 #define max(a,b) (((a) > (b)) ? (a) : (b))
@@ -45,47 +53,51 @@ using namespace std;
 
 class Solution {
 public:
-	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-		int n = nums1.size();
-		int m = nums2.size();
-
-		if (n > m)  //保证数组1一定最短
-		{
-			return findMedianSortedArrays(nums2, nums1);
-		}
-
-		// Ci 为第i个数组的割,比如C1为2时表示第1个数组只有2个元素。LMaxi为第i个数组割后的左元素。RMini为第i个数组割后的右元素。
-		int LMax1, LMax2, RMin1, RMin2, c1, c2, lo = 0, hi = 2 * n;  //我们目前是虚拟加了'#'所以数组1是2*n长度
-
-		while (lo <= hi)   //二分
-		{
-			c1 = (lo + hi) / 2;  //c1是二分的结果
-			c2 = m + n - c1;
-
-			LMax1 = (c1 == 0) ? INT_MIN : nums1[(c1 - 1) / 2];
-			RMin1 = (c1 == 2 * n) ? INT_MAX : nums1[c1 / 2];
-			LMax2 = (c2 == 0) ? INT_MIN : nums2[(c2 - 1) / 2];
-			RMin2 = (c2 == 2 * m) ? INT_MAX : nums2[c2 / 2];
-
-			if (LMax1 > RMin2)
-				hi = c1 - 1;
-			else if (LMax2 > RMin1)
-				lo = c1 + 1;
-			else
-				break;
-		}
-		return (max(LMax1, LMax2) + min(RMin1, RMin2)) / 2.0;
-	}
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(), m = nums2.size();
+        if (n > m) {
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        
+        int lMax1,lMax2,rMin1,rMin2,c1,c2,lo = 0, li = 2 * n;
+        
+        while (lo <= li) {
+            c1 = (lo + li) / 2;
+            c2 = m + n - c1;
+            lMax1 = (c1 == 0) ? INT_MIN : nums1.at( (c1-1)/2 );
+            rMin1 = (c1 == 2 * n) ? INT_MAX : nums1.at( c1/2 );
+            lMax2 = (c2 == 0) ? INT_MIN : nums2.at( (c2-1)/2 );
+            rMin2 = (c2 == 2 * m) ? INT_MAX : nums2.at( c2/2 );
+            
+            if (lMax1 > rMin2) {
+                li = c1 - 1;
+            } else if (lMax2 > rMin1) {
+                lo = c1 + 1;
+            } else {
+                break;
+            }
+        }
+        
+        return (max(lMax1, lMax2) + min(rMin1, rMin2)) / 2.0;
+    }
 };
 
+int main () {
+    int n , m ;
+    while (cin >> n >> m) {
+        vector<int> nums1;
+        int temp;
+        for (int i = 0 ; i < n; i ++) {
+            cin >> temp;
+            nums1.push_back(temp);
+        }
 
-int main(int argc, char *argv[])
-{
-	vector<int> nums1 = { 2,3, 5 };
-	vector<int> nums2 = { 1,4,7, 9 };
-	
-	Solution solution;
-	double ret = solution.findMedianSortedArrays(nums1, nums2);
-    cout << ret << endl;
-	return 0;
+        vector<int> nums2;
+        for (int i = 0 ; i < m; i ++) {
+            cin >> temp;
+            nums2.push_back(temp);
+        }
+        double ret = Solution().findMedianSortedArrays(nums1,nums2); 
+        cout  << setiosflags(ios::fixed) << setprecision(1) << ret << endl;
+    }
 }
